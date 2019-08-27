60c652700f1e1bc40e858ad6c3c187f162cc9b89
myllenadias <48135662+myllenadias@users.noreply.github.com>
2019-08-27 11:14:30 -0300

    Add files via upload

---
+++ conexao.php
@@ -0,0 +1,16 @@
+<?php
+// usado para conectar ao BD
+$host = "localhost";
+$db_name = "pdo_crud_01_bd_2info3";
+$username = "aluno";
+$password = "aluno";
+
+try {
+$con = new PDO("mysql:host={$host};dbname={$db_name}", $username,
+$password);
+}
+// mostra os erros
+catch(PDOException $exception){
+echo "Connection error: " . $exception->getMessage();
+}
+?>
